ae9d927327dce2181f3fd188af0927876e5e24af
VuthivannMemo <vuthivann.memo@gmail.com>
2024-04-02 18:48:13 +0700

    feat: add dark mode

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
+import { ModeToggle } from "@/components/mode-toggle";
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <main className="">
       <h1>Nextjs x Sanity Blog</h1>
-      <Button>Click me</Button>
+      <ModeToggle />
     </main>
   );
 }
