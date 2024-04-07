4a8ffc0ebaf1b1231414bfeadf8199cc9aea90ee
vuthivannyong <vuthivann.yong007@gmail.com>
2024-04-07 20:33:59 +0700

    feat(meta): config metadata

--- src/app/(user)/[...slug]/page.tsx
+++ src/app/(user)/[...slug]/page.tsx
@@ -1,6 +1,22 @@
+import { Metadata } from "next";
+
 import { postDetailQuery } from "@/lib/query";
 import Detail from "@/components/detail";
 import { client } from "../../../../sanity.client";
+
+type Props = {
+  params: { slug: string };
+};
+
+export const generateMetadata = async (props: Props): Promise<Metadata> => {
+  const { params } = props;
+  const post: IPost = await client.fetch(postDetailQuery, {
+    slug: params.slug.toString(),
+  });
+  return {
+    title: post.title,
+  };
+};
 
 export default async function PostDetailPage({
   params,
